762cbb4fcb62bdb78c656530a8d112ee673c14a3
Kithin Yeung <kithin.yeung@hotmail.com>
2016-02-15 17:08:35 +0100

    update

---
+++ SecondHandBuy/HomeScreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  SecondHandBuy
+//
+//  Created by Kithin Yeung on 15-02-16.
+//  Copyright © 2016 Kithin en Stephanie. All rights reserved.
+//
+
+import UIKit
+
+
+class HomeScreenViewController: UIViewController {
+    
+    
+    
+// MARK: Actions
+    @IBAction func clickToMapScreen(sender: AnyObject) {
+    }
+    
+}
---
+++ SecondHandBuy/PictureManager.swift
@@ -0,0 +1,9 @@
+//
+//  PictureManager.swift
+//  SecondHandBuy
+//
+//  Created by Kithin Yeung on 15-02-16.
+//  Copyright © 2016 Kithin en Stephanie. All rights reserved.
+//
+
+import UIKit
