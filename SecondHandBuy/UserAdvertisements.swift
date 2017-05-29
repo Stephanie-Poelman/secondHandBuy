b919a1ccb5664892eff93b88d25e76eaebc2f926
Stephanie <sl.poelman@gmail.com>
2017-05-29 15:05:56 +0200

    First commit

---
+++ SecondHandBuy/UserAdvertisements.swift
@@ -0,0 +1,9 @@
+//
+//  UserAdvertisements.swift
+//  SecondHandBuy
+//
+//  Created by Kithin Yeung on 23-04-16.
+//  Copyright © 2016 Kithin en Stephanie. All rights reserved.
+//
+
+import Foundation
